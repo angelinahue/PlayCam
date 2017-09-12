d3c095ee919cd425e45016b5319b4b6b9a7bc0d4
angelinahue <30992315+angelinahue@users.noreply.github.com>
2017-09-12 16:46:33 -0700

    Update PlayCamTests.swift

--- PlayCamTests/PlayCamTests.swift
+++ PlayCamTests/PlayCamTests.swift
@@ -1,9 +1,5 @@
 //
 //  PlayCamTests.swift
-//  PlayCamTests
-//
-//  Created by Mary Douglass Baum on 3/16/17.
-//  Copyright © 2017 anngbaum. All rights reserved.
 //
 
 import XCTest
@@ -13,23 +9,23 @@ class PlayCamTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // This is for an example of a functional test case.
+        // Use XCTAssert and related functions to verify the tests produce the correct results.
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
+        // This is for an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            // Measure the time here.
         }
     }
     
